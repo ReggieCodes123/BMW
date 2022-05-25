63ac86a83a5baea93573e7c47a8be9ebc7e4f23f
Reginald Johnson <johnsonreginald12@yahoo.com>
2022-05-24 21:07:45 -0400

    i forgot

--- BMW/Controller/LoginViewController.swift
+++ BMW/Controller/LoginViewController.swift
@@ -6,19 +6,36 @@
 //
 
 import UIKit
+import Parse
 
 class LoginViewController: UIViewController {
-
+    @IBOutlet weak var PasswordField: UITextField!
+    
+    @IBOutlet weak var UsernameField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
+    @IBAction func LoginButton(_ sender: Any) {
+        let Username = UsernameField.text ?? ""
+        let Password = PasswordField.text ?? ""
+        PFUser.logInWithUsername(inBackground: Username, password: Password){(user: PFUser?, error: Error?) in
+            if let error = error {
+                print("User log in failed: \(error.localizedDescription)")
+            } else {
+                print("user logged in successfully")
+                //display view controller that needs to shown after successful login
+            }
+        }
+    }
+    
     @IBAction func onInvestorSelect(_ sender: Any) {
         print("Hey")
     }
+    
     /*
     // MARK: - Navigation
 
